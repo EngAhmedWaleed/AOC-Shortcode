07e7738e542ba32ff8c1431b2f4baabc2648dcd8
EngAhmedWaleed <ahmedwaleed@csed22.com>
2020-08-30 13:05:15 +0200

    workin

--- bbp-do-shortcodes.php
+++ bbp-do-shortcodes.php
@@ -21,6 +21,7 @@ function pw_bbp_shortcodes($content, $reply_id)
 
 	return $content;
 }
+
 add_filter('bbp_get_reply_content', 'pw_bbp_shortcodes', 10, 2);
 add_filter('bbp_get_topic_content', 'pw_bbp_shortcodes', 10, 2);
 
@@ -33,54 +34,15 @@ function disable_bad_shortcodes($content)
 {
 
 	preg_match_all("/\[(\/)?[a-zA-Z\-\_]+(\s(.*))?\]/", $content, $matches);
-	//preg_match('/(foo)(bar)(baz)/', 'foobarbaz', $matches, PREG_OFFSET_CAPTURE);
-	//preg_replace("\[(\/)?[a-zA-Z\-\_]+(\s(.*))?\]","Matched",$content)
-
-	//$content_split = str_split($content);
-
-
-	/*for ($i = 0; $i <= count($content_split); $i++) {
-		if ($content_split[$i] == "[")
-			$end_bracket = find_end_bracket($content_split, ($i+1));
-	}*/
-
-	/*$str = "Result: ";
-
-	$str .= "<br> " . $matches[0] . " -> ";
-	foreach ($matches[0] as $in) {
-		$str .= $in . ", ";
-	}*/
 
 	$detected_shortcodes = $matches[0];
 
 	foreach ($detected_shortcodes as $in_bracket)
-		if (contain_shortcode($in_bracket))
+		if (contain_shortcode($in_bracket)) {
 			$content = str_replace($in_bracket, substr($in_bracket, 1, strlen($in_bracket) - 2), $content);
+		}
 
 	return $content;
-}
-
-function find_end_bracket($str_array, $j)
-{
-
-	for ($i = $j; $i <= count($str_array); $i++) {
-		if ($str_array[$i] == "]")
-			return $i;
-	}
-
-	return -1;
-}
-
-function check_inside_brackets($str_array, $i)
-{
-	if ($str_array[$i] == "/")
-		$i++;
-
-	//!ctype_alpha($str_array[$i+1]) has the same functionality.
-	if (!(preg_match("/^[a-zA-Z]$/", $str_array[$i]))) {
-		$str_array[$i] = "x";
-		return $str_array;
-	}
 }
 
 // https://artiss.blog/2017/07/listing-all-the-currently-available-wordpress-shortcodes/#:~:text=Add%20the%20following%20code%20to,dirty'%20but%20does%20the%20job.&text=add_shortcode(%20'shortcodes'%2C%20'list_shortcodes'%20)%3B
@@ -90,24 +52,24 @@ function contain_shortcode($in_bracket)
 	global $shortcode_tags;
 	$shortcodes = $shortcode_tags;
 
+	$in_bracket = strtolower($in_bracket);
+	$in_bracket = str_replace("/", "", $in_bracket);
+
+	//Allowed ones.
+	if(contains("code", $in_bracket))
+		return false;
+
 	foreach ($shortcodes as $code => $function) {
-		if ($shortcode == $code)
+		if (contains($code, $in_bracket))
 			return true;
 	}
 
 	return false;
 }
 
-function check_shortcode($shortcode)
+// https://stackoverflow.com/questions/4366730/how-do-i-check-if-a-string-contains-a-specific-word
+// returns true if $needle is a substring of $haystack
+function contains($needle, $haystack)
 {
-	// Grab the global array of shortcodes
-	global $shortcode_tags;
-	$shortcodes = $shortcode_tags;
-
-	foreach ($shortcodes as $code => $function) {
-		if ($shortcode == $code)
-			return true;
-	}
-
-	return false;
+	return strpos($haystack, $needle) !== false;
 }
