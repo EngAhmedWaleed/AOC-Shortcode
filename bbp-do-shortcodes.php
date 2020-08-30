8428c2d9e9e0e43453fa8330d861fda0a7cff160
EngAhmedWaleed <ahmedwaleed@csed22.com>
2020-08-30 12:46:04 +0200

    before clean up

--- bbp-do-shortcodes.php
+++ bbp-do-shortcodes.php
@@ -32,24 +32,36 @@ function pw_bbp_parse_capability()
 function disable_bad_shortcodes($content)
 {
 
-	$content_split = str_split($content);
-	$content = "";
+	preg_match_all("/\[(\/)?[a-zA-Z\-\_]+(\s(.*))?\]/", $content, $matches);
+	//preg_match('/(foo)(bar)(baz)/', 'foobarbaz', $matches, PREG_OFFSET_CAPTURE);
+	//preg_replace("\[(\/)?[a-zA-Z\-\_]+(\s(.*))?\]","Matched",$content)
 
-	for ($i = 0; $i <= count($content_split); $i++) {
+	//$content_split = str_split($content);
+
+
+	/*for ($i = 0; $i <= count($content_split); $i++) {
 		if ($content_split[$i] == "[")
 			$end_bracket = find_end_bracket($content_split, ($i+1));
+	}*/
 
-		if($end_bracket < 0)
-			$content .= $content_split[$i];
-		else
-			$content .= ("Here, ". $content_split[$i]);
+	/*$str = "Result: ";
 
-	}
+	$str .= "<br> " . $matches[0] . " -> ";
+	foreach ($matches[0] as $in) {
+		$str .= $in . ", ";
+	}*/
+
+	$detected_shortcodes = $matches[0];
+
+	foreach ($detected_shortcodes as $in_bracket)
+		if (contain_shortcode($in_bracket))
+			$content = str_replace($in_bracket, substr($in_bracket, 1, strlen($in_bracket) - 2), $content);
 
 	return $content;
 }
 
-function find_end_bracket($str_array, $j){
+function find_end_bracket($str_array, $j)
+{
 
 	for ($i = $j; $i <= count($str_array); $i++) {
 		if ($str_array[$i] == "]")
@@ -72,6 +84,20 @@ function check_inside_brackets($str_array, $i)
 }
 
 // https://artiss.blog/2017/07/listing-all-the-currently-available-wordpress-shortcodes/#:~:text=Add%20the%20following%20code%20to,dirty'%20but%20does%20the%20job.&text=add_shortcode(%20'shortcodes'%2C%20'list_shortcodes'%20)%3B
+function contain_shortcode($in_bracket)
+{
+	// Grab the global array of shortcodes
+	global $shortcode_tags;
+	$shortcodes = $shortcode_tags;
+
+	foreach ($shortcodes as $code => $function) {
+		if ($shortcode == $code)
+			return true;
+	}
+
+	return false;
+}
+
 function check_shortcode($shortcode)
 {
 	// Grab the global array of shortcodes
